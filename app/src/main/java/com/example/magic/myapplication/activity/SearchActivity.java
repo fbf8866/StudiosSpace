df73fee9ae022373805d0f440c047b1973f8a11a
fbf8866 <15101679907@139.com>
2017-10-25 16:41:52 +0800

    提交 // .... 测试 在建立分支的的

--- app/src/main/java/com/example/magic/myapplication/activity/SearchActivity.java
+++ app/src/main/java/com/example/magic/myapplication/activity/SearchActivity.java
@@ -53,7 +53,7 @@ public class SearchActivity extends BaseActivity{
                 if(TextUtils.isEmpty(editTextInput)){
                     MyToastUtils.show(SearchActivity.this,"请输入查询内容");
                      return;
-                } //分支1
+                } //分支12
                 Log.i(TAG, "onClick:输入内容 "+editTextInput);
                 cusrom_searchview.saveHistory(editTextInput);
                 cusrom_searchview.setEditTextInputNull();
@@ -65,7 +65,7 @@ public class SearchActivity extends BaseActivity{
                             .changeAlertType(SweetAlertDialog.ERROR_TYPE);
                 }
             }
-        }); //哈哈 分支 ,.....
+        }); //哈哈 分支 ,.....12
         toolbar.leftButtonOnClick(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
